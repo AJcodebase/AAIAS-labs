e43e7d7e871a3c63df4015df42e7a89e01812ef2
Aleric <alericpinto07@gmail.com>
2026-08-14 14:55:19 +0530

    tic tac toe additions

--- Minimax/Runner.py
+++ Minimax/Runner.py
@@ -2,3 +2,4 @@ import tic_tac_toe
 
 board = tic_tac_toe.initial_state()
 print(board)
+print(tic_tac_toe.player(board))
--- Minimax/tic_tac_toe.py
+++ Minimax/tic_tac_toe.py
@@ -10,3 +10,17 @@ def initial_state():
         [EMPTY, EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY]  
     ]
+
+def player(board):
+    x_count = 0
+    o_count = 0
+    for row in board:
+        for cell in row:
+            if cell == X:
+                x_count += 1
+            elif cell == O:
+                o_count += 1
+    if x_count <= o_count:
+        return X
+    else:
+        return O

298011022fcc11cffb31c313e33e16026ed82ee8
Aleric <alericpinto07@gmail.com>
2026-08-14 14:51:25 +0530

    Initial commit for AAIAS

---
+++ Minimax/Runner.py
@@ -0,0 +1,4 @@
+import tic_tac_toe
+
+board = tic_tac_toe.initial_state()
+print(board)
---
+++ Minimax/tic_tac_toe.py
@@ -0,0 +1,12 @@
+print("hello from tic-tac-toe")
+
+X = "X"
+O = "O"
+EMPTY = None
+
+def initial_state():
+    return [
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY]  
+    ]

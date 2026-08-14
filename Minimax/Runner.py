af7f58e4441e56f71228b24223084ef746209b82
Aleric <alericpinto07@gmail.com>
2026-08-14 15:45:45 +0530

    tic tac toe completetion

--- Minimax/Runner.py
+++ Minimax/Runner.py
@@ -1,13 +1,58 @@
+# import tic_tac_toe
+
+# board = tic_tac_toe.initial_state()
+# print("Board:", board)
+# print("Current player: ", tic_tac_toe.player(board))
+# print("Available actions: ", tic_tac_toe.actions(board))
+
+# move = (1,2)
+
+# new_board = tic_tac_toe.result(board, move)
+# print("New Board: ")
+# for row in new_board:
+#     print(row)
+
 import tic_tac_toe
+def print_board(board):
+    for row in board:
+        print(" | ".join(" "if cell is None else cell for cell in row))
+        print("---------")
+
 
 board = tic_tac_toe.initial_state()
-print("Board:", board)
-print("Current player: ", tic_tac_toe.player(board))
-print("Available actions: ", tic_tac_toe.actions(board))
 
-move = (1,2)
+print("Initial Board:")
+print_board(board)
 
-new_board = tic_tac_toe.result(board, move)
-print("New Board: ")
-for row in new_board:
-    print(row)
+while True:
+    print_board(board)
+    if tic_tac_toe.terminal(board):
+ 
+        break
+    if tic_tac_toe.player(board) == tic_tac_toe .X:
+        print("Your turn")
+        
+        row = int(input("Enter row (0-2): "))
+        if row < 0 or row > 2:
+            print("Invalid row. Please enter a value between 0 and 2.")
+            continue
+        column = int(input("Enter column (0-2): "))
+        if column < 0 or column > 2:
+            print("Invalid column. Please enter a value between 0 and 2.")
+            continue
+        move = (row, column)
+        board = tic_tac_toe.result(board, move)
+    else:
+        print("AI is thinking...")
+        ai_move = tic_tac_toe.minimax(board)
+        board = tic_tac_toe .result(board, ai_move)
+print_board(board)
+winner = tic_tac_toe.winner(board)
+if winner == tic_tac_toe    .X:
+ 
+    print("X wins!")
+elif winner == tic_tac_toe.O:
+ 
+    print("O wins!")
+else:
+    print("Draw!")
--- Minimax/tic_tac_toe.py
+++ Minimax/tic_tac_toe.py
@@ -1,3 +1,5 @@
+import math
+
 X = "X"
 O = "O"
 EMPTY = None
@@ -43,3 +45,95 @@ def result(board, action):
     return new_board
 
 def winner(board):
+    for i in range(3):
+        if board[i][0] != EMPTY:
+            if board[i][0] == board[i][1] == board[i][2]:
+                return board[i][0]
+ 
+    for j in range(3):
+        if board[0][j] != EMPTY:
+            if board[0][j] == board[1][j] == board[2][j]:
+                return board[0][j]
+    if board[0][0] != EMPTY: 
+        if board[0][0] == board[1][1] == board[2][2]:
+            return board[0][0]
+    # Check second diagonal
+    if board[0][2] != EMPTY:
+        if board[0][2] == board[1][1] == board[2][0]:
+            return board[0][2]
+    return None
+ 
+ 
+ 
+ 
+ 
+def terminal(board):
+    # Someone won
+    if winner(board) is not None:
+        return True 
+    for row in board:
+        for cell in row:
+            if cell == EMPTY:
+                return False 
+    return True
+ 
+ 
+ 
+ 
+ 
+def utility(board):
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    if terminal(board):
+        return None
+    if player(board) == X:
+        best_score = -math.inf
+        best_action = None
+        for action in actions(board):
+            new_board = result(board, action)
+            score = minimax_value(new_board)
+            if score > best_score:
+                best_score = score
+                best_action = action
+        return best_action
+    else:
+        best_score = math.inf
+        best_action = None
+        for action in actions(board):
+            new_board = result(board, action)
+            score = minimax_value(new_board)
+            if score < best_score:
+                best_score = score
+                best_action = action
+        return best_action
+ 
+ 
+ 
+ 
+ 
+def minimax_value(board):
+    if terminal(board):
+        return utility(board)
+    if player(board) == X:
+        best_score = -math.inf
+        for action in actions(board):
+            new_board = result(board, action)
+            score = minimax_value(new_board)
+            best_score = max(best_score, score)
+        return best_score
+    else:
+        best_score = math.inf
+        for action in actions(board):
+            new_board = result(board, action)
+            score = minimax_value(new_board)
+            best_score = min(best_score, score)
+        return best_score
+ 
